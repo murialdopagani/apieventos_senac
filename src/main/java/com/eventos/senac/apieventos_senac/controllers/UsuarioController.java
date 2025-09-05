d98df2344c132a1c7d5c0bed7d07d5138cb066ac
murialdopagani <murialdo.pagani@gmail.com>
2025-09-05 15:18:03 -0300

    Ajustes na classe UsuarioController

--- src/main/java/com/eventos/senac/apieventos_senac/controllers/UsuarioController.java
+++ src/main/java/com/eventos/senac/apieventos_senac/controllers/UsuarioController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,10 +22,9 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @PostMapping(value = "/salvarUsuario")
+    @PostMapping(value = "/salvar")
     @Operation(summary = "Salva um Usuário", description = "Método responsável por salvar um Usuário no banco de dados.")
     public ResponseEntity<UsuarioResponseDto> salvarUsuario(@RequestBody UsuarioCriarRequestDto usuarioRequestDto) {
-
         try {
             Usuario usuarioSalvo = usuarioRepository.save(new Usuario(usuarioRequestDto));
             //UsuarioResponseDto usuarioResponseDto = new UsuarioResponseDto(usuarioSalvo);
@@ -38,58 +36,46 @@ public class UsuarioController {
             System.err.println("Erro ao salvar usuário: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        //endregion
-
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Consulta de usuário por id",
             description = "Método responsável por consultar um único usuário por id, se não existir retorna null")
     public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
-
         var usuario = usuarioRepository.findById(id).orElse(null);
         return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/listar")
+    @Operation(summary = "Listar todos", description = "Método responsável por listar todos os usuários cadastrados")
+    public ResponseEntity<List<UsuarioResponseDto>> listar() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        List<UsuarioResponseDto> usuarioResponseDto = usuarios.stream()
+                .map(UsuarioResponseDto::fromUsuario)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(usuarioResponseDto);
+
+        //        ArrayList<UsuarioResponseDto> usuarioResponseDto = new ArrayList<>();
+        //        for (Usuario usuario : usuarios) {
+        //            usuarioResponseDto.add(UsuarioResponseDto.fromUsuario(usuario));
+        //        }
+        //        return ResponseEntity.ok(usuarioResponseDto);
+
+        //        List<UsuarioResponseDto> usuarioResponseDto = new ArrayList<>();
+        //        usuarios.forEach(usuarioResponse -> {
+        //            usuarioResponseDto.add(UsuarioResponseDto.fromUsuario(usuarioResponse));
+        //        });
+        //        return ResponseEntity.ok(usuarioResponseDto);
 
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Deletar usuário por id", description = "Método para deletar um usuário por id")
     public ResponseEntity<?> deletarPoId(@PathVariable Long id) {
-
         usuarioRepository.deleteById(id);
         return ResponseEntity.ok().build();
-
     }
-
-    @GetMapping("/listar")
-    public ResponseEntity<List<UsuarioResponseDto>> listarUsuarios() {
-
-        ArrayList<Usuario> usuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
-
-
-        ArrayList<UsuarioResponseDto> usuarioResponseDto0 = new ArrayList<>();
-        for (Usuario usuario : usuarios) {
-            usuarioResponseDto0.add(UsuarioResponseDto.fromUsuario(usuario));
-        }
-
-        ArrayList<UsuarioResponseDto> usuarioResponseDto1 = new ArrayList<>();
-        usuarios.forEach(usuarioResponse -> {
-            usuarioResponseDto1.add(UsuarioResponseDto.fromUsuario(usuarioResponse));
-        });
-
-        List<UsuarioResponseDto> usuarioResponseDto = usuarios.stream()
-                .map(UsuarioResponseDto::fromUsuario)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(usuarioResponseDto0);
-
-    }
-
-    //    @PostMapping
-    //    @Operation(summary = "Criar usuário", description = "Método para criar um usuário")
-    //    public ResponseEntity<?> criarUsuario(@RequestBody UsuarioCriarRequestDto usuario){
-    //
-    //        return  ResponseEntity.ok(new Usuario(usuario));
-    //    }
 
 }
--- src/main/java/com/eventos/senac/apieventos_senac/repository/UsuarioRepository.java
+++ src/main/java/com/eventos/senac/apieventos_senac/repository/UsuarioRepository.java
@@ -2,11 +2,12 @@ package com.eventos.senac.apieventos_senac.repository;
 
 import com.eventos.senac.apieventos_senac.model.entity.Usuario;
 import org.hibernate.annotations.processing.SQL;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsUsuarioByCpf_CpfAndSenhaContaining(String Cpf , String Senha);
 
