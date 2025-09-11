68a2954ab9aca123a79eb77d7a772e26266aaa31
murialdopagani <murialdo.pagani@gmail.com>
2025-09-11 16:48:42 -0300

    Ajuste no nome do atributo Cpf para cpf

--- src/main/java/com/eventos/senac/apieventos_senac/dto/UsuarioCriarRequestDto.java
+++ src/main/java/com/eventos/senac/apieventos_senac/dto/UsuarioCriarRequestDto.java
@@ -4,6 +4,6 @@ import com.eventos.senac.apieventos_senac.model.entity.Evento;
 
 import java.util.List;
 
-public record UsuarioCriarRequestDto(String nome, String email, String senha, String Cpf, String telefone){
+public record UsuarioCriarRequestDto(String nome, String email, String senha, String cpf, String telefone){
 
 }
